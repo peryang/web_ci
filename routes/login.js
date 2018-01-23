dd15f29a9ab84baeb578481b73f7564023d26033
bjyangxiuwu <bjyangxiuwu@corp.netease.com>
2018-01-23 11:16:56 +0800

    post校验

--- routes/login.js
+++ routes/login.js
@@ -3,24 +3,25 @@ var router = express.Router();
 var users = require("../database/users")["users"];
 var parseResponse = require("../func/parseResponse");
 
-router.post('/', function (req, res) {
+router.post('/', function(req, res, next) {
 	var userName = req.query.username;
 	var psd = req.query.password;
-  if (!userName || !psd) {
-  	res.send(parseResponse.error(-1, "", "login failed"));
-  }else{
-  	if(!users[userName]){
-  		res.send(parseResponse.error("-2", "", "user does not exist"));
-  	}else{
-  		if(psd === users[userName]) {
-		    req.session.user = userName;
-		    res.send(parseResponse.success(1, "", "login success!"));
-		  }else{
-		  	parseResponse.error(code, data, msg)
-		  	res.send(parseResponse.error(-1, "", "password error!"));
+	console.log(userName, psd);
+	if (!userName || !psd) {
+		res.send(parseResponse.error(-1, "", "login failed"));
+	}else{
+		if(!users[userName]){
+			res.send(parseResponse.error("-2", "", "user does not exist"));
+		}else{
+			if(psd === users[userName]) {
+				req.session.user = userName;
+				res.send(parseResponse.success(1, "", "login success!"));
+			}else{
+				parseResponse.error(code, data, msg)
+				res.send(parseResponse.error(-1, "", "password error!"));
+			}
 		}
-  	}
-  }
+	}
 });
 
 module.exports = router;
